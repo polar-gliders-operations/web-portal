5a8e3cebfb302f87fd2e1dc13059befe93ab1225
isgiddy <isgiddy@gmail.com>
2021-12-30 14:58:40 +0100

    update directories

--- data-load/move_files.sh
+++ data-load/move_files.sh
@@ -1,6 +1,6 @@
 cp asi* seaice_latest.hdf
-mv *.hdf /home/mduplessis/web-portal/data/
-mv nrt*.nc /home/mduplessis/web-portal/data/adt_latest.nc
-mv sst*.nc /home/mduplessis/web-portal/data/
-mv wind*.nc /home/mduplessis/web-portal/data/
-mv Long*.hdf /home/mduplessis/web-portal/data/
+mv *.hdf /home/web/web-portal/data/
+mv nrt*.nc /home/web/web-portal/data/adt_latest.nc
+mv sst*.nc /home/web/web-portal/data/
+mv wind*.nc /home/web/web-portal/data/
+mv Long*.hdf /home/web/web-portal/data/
