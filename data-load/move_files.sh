000dec889006f3b1f0a4dfd6028772a159ae581f
isgiddy <isgiddy@gmail.com>
2022-01-07 14:16:58 +0100

    comment

--- data-load/move_files.sh
+++ data-load/move_files.sh
@@ -3,4 +3,4 @@ mv *.hdf /home/web/web-portal/data/
 mv nrt*.nc /home/web/web-portal/data/adt_latest.nc
 mv sst*.nc /home/web/web-portal/data/
 mv wind*.nc /home/web/web-portal/data/
-mv Long*.hdf /home/web/web-portal/data/
+#mv Long*.hdf /home/web/web-portal/data/
